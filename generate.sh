8fad4e9d67d4ae17f546aedc7b206963ac6913d8
akhilrs <hello@akhil.rs>
2021-02-09 08:07:50 +0530

    fix: add latest tag to target group

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 set -e
 
 DOCKER_BAKE_FILE=${1:-"docker-bake.hcl"}
-TAGS=${TAGS:-"latest 3.1.3.0"}
+TAGS=${TAGS:-"3.1.3.0"}
 FILE_LOCATION=${FILE_LOCATION:-"https://ispyfiles.azureedge.net/downloads/Agent_Linux64_3_1_3_0.zip"}
 DEFAULT_FILE_LOCATION=${DEFAULT_FILE_LOCATION:-"https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=Linux"}
 TZ=${TZ:-"Asia/Kolkata"}
@@ -18,7 +18,6 @@ P="\"$(echo $PLATFORMS | sed 's/ /", "/g')\""
 
 T="\"$(echo $MAIN_TAG)\", \"$(echo $TAGS_EXTRA | sed 's/ /", "/g')\""
 
-echo $T
 
 
 cat > "$DOCKER_BAKE_FILE" << EOF
@@ -30,12 +29,15 @@ target "common" {
 	args = {"FILE_LOCATION" = "$FILE_LOCATION", "DEFAULT_FILE_LOCATION" = "$DEFAULT_FILE_LOCATION", "TZ" = "$TZ"}
     dockerfile = "Dockerfile"
 }
+target "latest" {
+    inherits = ["common"]
+    tags = ["$IMAGE_NAME:latest"]
+}
 EOF
 
 for TAG in $TAGS_EXTRA; do cat >> "$DOCKER_BAKE_FILE" << EOF
 target "$TAG" {
   inherits = ["common"]
-	args = {"BASETAG" = "$TAG"}
   tags = ["$IMAGE_NAME:$TAG"]
 }
 EOF
